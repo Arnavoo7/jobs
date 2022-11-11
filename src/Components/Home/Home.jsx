837cb92fd2c012082a7f5b799d35afe0920157fc
Arnab Mahato <arnabmahato1010@gmail.com>
2022-11-11 16:50:53 +0530

    structural changes

--- src/Components/Home/Home.jsx
+++ src/Components/Home/Home.jsx
@@ -15,22 +15,22 @@ const Home = () => {
           <h1>IT Jobs</h1>
 
           <div className="section">
+  
+            <button className="subSections3" onClick={() => { navigate("/nocriteria") }}>
+              <h2>IT Jobs</h2>   
+            </button>
 
-            <button className="subSections1" onClick={() => { navigate("/alljobs"); }}>
-              <h2>All jobs</h2>
+            {/* <button className="subSections1" onClick={() => { navigate("/alljobs"); }}>
+              <h2>IT jobs</h2>
             </button>
 
             <button className="subSections2" onClick={() => { navigate("/companywise") }}>
               <h2>Company wise jobs</h2>
             </button>
   
-            <button className="subSections3" onClick={() => { navigate("/nocriteria") }}>
-              <h2>50 or no % criteria</h2>   
-            </button>
-  
             <button className="subSections4" onClick={() => { navigate("/others"); }}>
               <h2>others</h2>   
-            </button>
+            </button> */}
            
           </div>
           
